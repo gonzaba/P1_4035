8711b2fc4948e60f03595941017796f763a8889f
Barbara P. Gonzalez-Rivera <barbara.gonzalez1@upr.edu>
2018-03-10 15:23:31 -0400

    Updated P3 and P4

--- P1_802142976/src/p1MainClasses/P3.java
+++ P1_802142976/src/p1MainClasses/P3.java
@@ -1,5 +1,19 @@
 package p1MainClasses;
 
-public class P3 {
+import interfaces.MySet;
+import setIntersectionFinders.AbstractIntersectionFinder;
+
+public class P3 extends AbstractIntersectionFinder {
+
+	public P3(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public MySet intersectSets(MySet[] t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- P1_802142976/src/p1MainClasses/P4.java
+++ P1_802142976/src/p1MainClasses/P4.java
@@ -1,5 +1,19 @@
 package p1MainClasses;
 
-public class P4 {
+import interfaces.MySet;
+import setIntersectionFinders.AbstractIntersectionFinder;
+
+public class P4 extends AbstractIntersectionFinder {
+
+	public P4(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public MySet intersectSets(MySet[] t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
