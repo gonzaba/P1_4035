6bcc3cb49b857f11a8401ba59a44f136b30de063
Barbara P. Gonzalez-Rivera <barbara.gonzalez1@upr.edu>
2018-03-10 15:23:07 -0400

    Added Part1Main, Part2Main and P1andP2, P3 and P4 classes.
    All are empty

---
+++ P1_802142976/src/p1MainClasses/P1andP2.java
@@ -0,0 +1,21 @@
+package p1MainClasses;
+
+import interfaces.MySet;
+import setIntersectionFinders.AbstractIntersectionFinder;
+
+public class P1andP2 extends AbstractIntersectionFinder {
+
+	public P1andP2(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public MySet intersectSets(MySet[] t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
