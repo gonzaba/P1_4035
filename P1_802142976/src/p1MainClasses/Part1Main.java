1901b0e2a62b770710c1cfeaf178cf56e3cce66a
Barbara P. Gonzalez-Rivera <barbara.gonzalez1@upr.edu>
2018-03-11 19:37:38 -0400

    added parts to part1main

--- P1_802142976/src/p1MainClasses/Part1Main.java
+++ P1_802142976/src/p1MainClasses/Part1Main.java
@@ -1,5 +1,8 @@
 package p1MainClasses;
 
+import java.io.FileNotFoundException;
+
+import dataGenerator.DataReader;
 
 /**
  * 
@@ -13,10 +16,27 @@ package p1MainClasses;
 
 public class Part1Main {
 
-	  public static void main(String[] args) {
+	  public static void main(String[] args) throws FileNotFoundException {
 		  
+		DataReader dr = new DataReader();
 		
-		  
+		//Calling the FilesGeneratorMain to read the files
+		FilesGeneratorMain fgm = new FilesGeneratorMain();
+		
+		//tells the FilesGeneratorMain to read the arguments given by user 
+		//when calling part1main by cmd and giving the numbers
+		//FilesGeneratorMain has already a method to read the arguments or assign a default value.
+		fgm.main(args);
+		
+		dr.readDataFiles();
+		
+		dr.printSets();
+		
+		//remember to change the argument
+		P1andP2 p1p2result = new P1andP2(null);
+		P3 p3result = new P3(null);
+		P4 p4result = new P4(null);
+		
 		  
 	  }
 }
--- P1_802142976/src/p1MainClasses/Part2Main.java
+++ P1_802142976/src/p1MainClasses/Part2Main.java
@@ -1,10 +1,19 @@
 package p1MainClasses;
 
+import java.io.FileNotFoundException;
+
+import dataGenerator.DataReader;
+
 /**
  * 
  * @author Barbara P. Gonzalez 802-14-2976
  *
  */
 public class Part2Main {
+		  public static void main(String[] args) throws FileNotFoundException {
+			  
+			  DataReader dr = new DataReader();
+			  
+		  }
 
 }
